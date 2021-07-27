55aba0c19140c6b0437a9b7adede3abb830e9d03
sonichigo <pathakanimesh12@gmail.com>
2021-07-27 16:24:31 +0530

    Heroku Deployment

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO, emit
 app = Flask(__name__)
-socket = SocketIO(app)
+
+socket = SocketIO(app,logger=True,engineio_logger=True)
 
 @app.route('/')
 def index():
@@ -10,14 +11,18 @@ def index():
 @socket.on('connect')
 def connect():
     print("[CLIENT CONNECTED]:", request.sid)
+
 @socket.on('disconnect')
 def disconn():
     print("[CLIENT DISCONNECTED]:", request.sid)
+
 @socket.on('notify')
 def notify(user):
     emit('notify', user, broadcast=True, skip_sid=request.sid)
+
 @socket.on('data')
 def emitback(data):
     emit('returndata', data, broadcast=True)
+    
 if __name__ == "__main__":
     socket.run(app)
