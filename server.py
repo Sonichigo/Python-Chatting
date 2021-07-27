e8cd4393bbeb1f1170bdf3d081a34932d8e1f734
sonichigo <pathakanimesh12@gmail.com>
2021-07-27 11:45:41 +0530

    New Room

--- server.py
+++ server.py
@@ -2,9 +2,11 @@ from flask import Flask, request, render_template
 from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 socket = SocketIO(app)
+
 @app.route('/')
 def index():
     return render_template('client.html')
+
 @socket.on('connect')
 def connect():
     print("[CLIENT CONNECTED]:", request.sid)
